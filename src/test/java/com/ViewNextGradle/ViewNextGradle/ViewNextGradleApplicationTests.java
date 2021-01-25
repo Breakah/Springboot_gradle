c355b06e5680a69482b3c6c7da93de7c7f435ac7
Manuel Jesus <mjvazquez@viewnext.com>
2021-01-25 15:27:19 +0100

    Commit inicial

---
+++ src/test/java/com/ViewNextGradle/ViewNextGradle/ViewNextGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViewNextGradle.ViewNextGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewNextGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
